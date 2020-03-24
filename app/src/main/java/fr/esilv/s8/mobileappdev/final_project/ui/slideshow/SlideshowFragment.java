e7d6b1babca061726c56edf481a8755c10ff0385
Adrien Turchini <adrien.turchini@me.com>
2020-03-25 00:45:21 +0100

    add pictures, modify display of weather

--- app/src/main/java/fr/esilv/s8/mobileappdev/final_project/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/fr/esilv/s8/mobileappdev/final_project/ui/slideshow/SlideshowFragment.java
@@ -20,18 +20,17 @@ import fr.esilv.s8.mobileappdev.final_project.ui.slideshow.AdapterSlideshow;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+
     private RecyclerView rv;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
         rv = (RecyclerView) root.findViewById(R.id.fragment_slideshow_rv);
-        int[] photo = new int[]{R.drawable.slide,R.drawable.guillaume, R.drawable.pauline,
-                R.drawable.loris, R.drawable.pierre, R.drawable.marius, R.drawable.melvyn};
+        int[] photo = new int[]{R.drawable.a11,R.drawable.a12, R.drawable.a13,
+                R.drawable.a14, R.drawable.a15, R.drawable.a16, R.drawable.a17, R.drawable.a18, R.drawable.slide, R.drawable.marius, R.drawable.melvyn, R.drawable.pierre, R.drawable.loris, R.drawable.pauline, R.drawable.guillaume, R.drawable.pierre};
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), 2, GridLayoutManager.VERTICAL, false);
         rv.setLayoutManager(gridLayoutManager);
